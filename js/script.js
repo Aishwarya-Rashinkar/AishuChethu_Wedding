3f3d8bebc7c0f333177726526d341f9d197dd635
Aishwarya Rashinkar <66772622+Aishwarya-Rashinkar@users.noreply.github.com>
2023-01-28 16:05:37 +0530

    FOURTH

--- js/script.js
+++ js/script.js
@@ -34,7 +34,7 @@ var x = setInterval(function() {
     
     // Output the result in an element with id="demo"
 
-    document.getElementById("time").innerHTML = "<div class='container'><div class='days block'></div>";
+    document.getElementById("time").innerHTML = "<div class='days block'></div>";
 
     // If the count down is over, write some text 
     if (distance < 0) {
