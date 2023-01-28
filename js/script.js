88c711d97aa8f2152a811472fdde6864490b6cb0
Aishwarya Rashinkar <66772622+Aishwarya-Rashinkar@users.noreply.github.com>
2023-01-28 16:02:43 +0530

    3RD

--- js/script.js
+++ js/script.js
@@ -30,16 +30,16 @@ var x = setInterval(function() {
     
     // Time calculations for days, hours, minutes and seconds
     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60 *24));
+    //var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60 *24));
     
     // Output the result in an element with id="demo"
 
-    document.getElementById("time").innerHTML = "<div class='container'><div class='days block'>"+ days + "<br>Days</div>" + "<div class='hours block'>" + hours + "<br>Hours</div></div>";
+    document.getElementById("time").innerHTML = "<div class='container'><div class='days block'></div>";
 
     // If the count down is over, write some text 
     if (distance < 0) {
         clearInterval(x);
-        document.getElementById("time").innerHTML = "Bless the married couple for happy life!";
+        document.getElementById("time").innerHTML = "HURRAY!!!";
     }
 }, 1000);
 
